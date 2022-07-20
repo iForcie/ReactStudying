702ab853244c1b0d9617670ba7178291130c283a
iForcie <ilyacool3@gmail.com>
2022-07-20 15:32:21 +0300

    Getting values of input (not adding new employee)

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,48 @@
+import { Component } from 'react';
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-	return (
-		<div className="app-add-form">
-			<h3>Добавьте нового сотрудника</h3>
-			<form 
-				className='add-form d-flex'>
-				<input type="text" 
-					className="form-control new-post-label"
-					placeholder="Как его зовут?" />
-				<input type="number" 
-					className="forn-control new-post-label"
-					placeholder="З/П в $?" />
+export default class EmployeesAddForm  extends Component {
 
-				<button type="submit"
-						className="btn btn-outline-light">Добавить</button>
-			</form>
-		</div>
-	);
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			salary: ''
+		}
+	}
+
+	onValueChange = (e) => {
+		this.setState({
+			[e.target.name]: e.target.value,
+			[e.target.salary]: e.target.value
+		});	
+	}
+
+	render() {
+		const {name, salary} = this.state;
+
+		return (
+			<div className="app-add-form">
+				<h3>Добавьте нового сотрудника</h3>
+				<form 
+					className='add-form d-flex'>
+					<input type="text" 
+						className="form-control new-post-label"
+						placeholder="Как его зовут?"
+						name="name"
+						value={name}
+						onChange={this.onValueChange}/>
+					<input type="number" 
+						className="forn-control new-post-label"
+						placeholder="З/П в $?" 
+						name="salary"
+						value={salary}
+						onChange={this.onValueChange}/>
+	
+					<button type="submit"
+							className="btn btn-outline-light">Добавить</button>
+				</form>
+			</div>
+		);
+	}
 };
-
-export default EmployeesAddForm;
